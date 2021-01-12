dcc08315733e8b4cecd1ce6e53bf845bd8a3df36
b177y <34008579+b177y@users.noreply.github.com>
2021-01-12 21:20:04 +0000

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MainCard from './components/maincard';
+
+function App() {
+  return (
+    <div className="App">
+        <MainCard />
+    </div>
+  );
+}
+
+export default App;
